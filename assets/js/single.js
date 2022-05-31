d67ba910431e8819251abc6d87b4e36518574662
Your Name <drjpolanco25@yahoo.com>
2022-05-31 14:42:57 -0700

    update

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,24 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function (){
+  //grab repo name form url query string
+ var queryString= document.location.search
+  var repoName= queryString.split("=")[1];
+  getRepoIssues(repoName);
+  repoNameEl.textContent= repoName;
+ if (repoName){
+   //display repo name on the page
+ repoNameEl.textContent = repoName;
+ getRepoIssues(repoName);
+ }
+ else{
+   //if no repo was given , redirect to the homepage
+   document.location.replace("./index.html");
+ }
+}
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -20,8 +38,8 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+     //if not succesful, redirect to homepage
+     document.location.replace("./index.html");
     }
   });
 };
@@ -81,4 +99,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
